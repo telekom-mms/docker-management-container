960bfb37cb6c5926259ebe38f770b081af401673
mima <Michaela.Mattes@t-systems.com>
2022-10-18 15:30:29 +0200

    add profile setup

--- render.sh
+++ render.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # generate Dockerfile from template with build settings
 
 # init stuff
@@ -11,10 +11,12 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 ENV_DIR=${1-SCRIPT_DIR}
 ENV_FILE="${ENV_DIR}/build.yaml"
 BUILD_FILE="${ENV_DIR}/.build"
+DOCKERFILE_TEMPLATE="${SCRIPT_DIR}/Dockerfile.template"
+DOCKERFILE="${ENV_DIR}/Dockerfile"
 
 # function
 parse_yaml() {
-  local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+  local s='[[:space:]]*' w='[.a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
   sed -ne "s|,$s\]$s\$|]|" \
       -e ":1;s|^\($s\)\($w\)$s:$s\[$s\(.*\)$s,$s\(.*\)$s\]|\1\2: [\3]\n\1  - \4|;t1" \
       -e "s|^\($s\)\($w\)$s:$s\[$s\(.*\)$s\]|\1\2:\n\1  - \3|;p" $1 | \
@@ -51,25 +53,40 @@ parse_yaml() {
   }'
 }
 
-render(){
-  echo > "${ENV_DIR}/Dockerfile"
-  parse_yaml ${ENV_FILE} > ${BUILD_FILE}
-
-  KEYS=$(cat ${BUILD_FILE} | cut -d '=' -f1 | sort -u)
-
-  for KEY in ${KEYS}
-  do
-    VALUE=$(cat ${BUILD_FILE} | grep -w ${KEY} | cut -d '=' -f2- | sort -u | tr '\n' ' ')
-    ARG=$(echo ${KEY} | awk '{print toupper($0)}')
-
-    sedStr="${sedStr} s!ARG \<${ARG}\>!ARG ${ARG}=\"${VALUE}\"!g;"
-  done
-
-  echo "${sedStr}"
-  rm ${BUILD_FILE}
-}
-
 # main
-SED_ARG=$(render)
+echo > ${DOCKERFILE}
+parse_yaml ${ENV_FILE} > ${BUILD_FILE}
 
-sed -r "${SED_ARG}" "${SCRIPT_DIR}/Dockerfile.template" > "${ENV_DIR}/Dockerfile"
+KEYS=$(cat ${BUILD_FILE} | cut -d '=' -f1 | sort -u)
+
+## generate ARG=VALUE for replacement
+for KEY in ${KEYS}
+do
+  VALUE=$(cat ${BUILD_FILE} | grep -w ${KEY} | cut -d '=' -f2- | sort -u | tr '\n' ';')
+  VALUE_PREFIX=","
+  ARG=$(echo ${KEY} | awk '{print toupper($0)}' | tr -d '.')
+
+  if [ "$(grep -w "ARG ${ARG}" ${DOCKERFILE_TEMPLATE} | wc -l)" -eq 0 ]; then
+    ARG_N=$(echo ${ARG} | awk -F '_' '{print $1"_N"}')
+    SED_APPEND_ARGS="${SED_APPEND_ARGS}#${ARG_N}=${ARG}=\"${VALUE}\""
+  else
+    SED_REPLACE="${SED_REPLACE} s!ARG \<${ARG}\>!ARG ${ARG}=\"${VALUE}\"!g;"
+  fi
+done
+
+## replace ARG
+sed -r "${SED_REPLACE}" ${DOCKERFILE_TEMPLATE} > ${DOCKERFILE}
+
+## append ARG
+if [ "${SED_APPEND_ARGS}" != "" ]; then
+  SED_APPEND_KEYS=$(echo $SED_APPEND_ARGS |  tr '#' '\n' | cut -d '=' -f1 | sort -u | xargs)
+
+  for SED_APPEND_KEY in ${SED_APPEND_KEYS}; do
+    SED_APPEND_KEY_ARG=$(echo ${SED_APPEND_ARGS} |  tr '#' '\n' | sed -n "s/${SED_APPEND_KEY}=/ARG /p" | sed 's/$/\\n/g' | tr -d '\n')
+
+
+    sed -i "/ARG ${SED_APPEND_KEY}/a ${SED_APPEND_KEY_ARG}" ${DOCKERFILE}
+  done
+fi
+
+rm ${BUILD_FILE}
