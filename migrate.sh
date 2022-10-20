aa47efafef5a3ebc9e2e801e8952df681926d315
mima <Michaela.Mattes@t-systems.com>
2022-10-20 13:06:44 +0200

    add more workflows

--- migrate.sh
+++ migrate.sh
@@ -7,8 +7,8 @@ if [ $# -ne 1 ]; then
   echo "Usage: sh migrate.sh <DIRECTORY>"
   exit 2
 fi
-SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-ENV_DIR=${1-SCRIPT_DIR}
+
+ENV_DIR=${1-$(dirname "$(readlink -f "$0")")}
 ENV_FILE="${ENV_DIR}/build.yaml"
 OLD_ENV_FILE="${ENV_DIR}/.docker_build"
 
@@ -20,106 +20,106 @@ EXTENSIONS="AZ_CLI_EXTENSIONS|HELM_EXTENSIONS"
 ## funtion
 migrate() {
   ## packages
-  SEARCH_PACKAGES=$(cat ${OLD_ENV_FILE} | grep -E ${PACKAGES} | sed 's/"//g' | sed 's/PACKAGES=//g')
+  SEARCH_PACKAGES=$(grep -E ${PACKAGES} "${OLD_ENV_FILE}" | sed 's/"//g' | sed 's/PACKAGES=//g')
   if [ "${SEARCH_PACKAGES}" != "" ]; then
-    echo "packages:" > ${ENV_FILE}
+    echo "packages:" > "${ENV_FILE}"
 
     for SEARCH_PACKAGE in ${SEARCH_PACKAGES}
     do
-      PACKAGE_RESULT=$(echo ${SEARCH_PACKAGE} | sed 's/_VERSION//g' |  sed 's/=latest//g' | awk '{print tolower($0)}')
-      PACKAGE=$(echo ${PACKAGE_RESULT} | sed 's/docker/docker-ce/g' | sed 's/az_cli/azure-cli/g' | sed 's/aws_cli/awscli/g')
-      echo "  - ${PACKAGE}" >> ${ENV_FILE}
+      PACKAGE_RESULT=$(echo "${SEARCH_PACKAGE}" | sed 's/_VERSION//g' |  sed 's/=latest//g' | awk '{print tolower($0)}')
+      PACKAGE=$(echo "${PACKAGE_RESULT}" | sed 's/docker/docker-ce/g' | sed 's/az_cli/azure-cli/g' | sed 's/aws_cli/awscli/g')
+      echo "  - ${PACKAGE}" >> "${ENV_FILE}"
     done
   fi
 
   ## repositories
-  SEARCH_REPOSITORIES=$(cat ${ENV_FILE} | grep -E "azure-cli|docker-ce|nomad|consul" | sed 's/-\ //g')
+  SEARCH_REPOSITORIES=$(grep -E "azure-cli|docker-ce|nomad|consul" "${ENV_FILE}" | sed 's/-\ //g')
   if [ "${SEARCH_REPOSITORIES}" != "" ]; then
-    echo "repositories:" >> ${ENV_FILE}
+    echo "repositories:" >> "${ENV_FILE}"
 
     for SEARCH_REPOSITORY in ${SEARCH_REPOSITORIES}
     do
-      REPOSITORY=$(echo ${SEARCH_REPOSITORY} | sed 's/azure-cli/microsoft/g' | sed 's/docker-ce/docker/g' | sed 's/nomad/hashicorp/g' | sed 's/consul/hashicorp/g')
-      echo "  - ${REPOSITORY}" >> ${ENV_FILE}
+      REPOSITORY=$(echo "${SEARCH_REPOSITORY}" | sed 's/azure-cli/microsoft/g' | sed 's/docker-ce/docker/g' | sed 's/nomad/hashicorp/g' | sed 's/consul/hashicorp/g')
+      echo "  - ${REPOSITORY}" >> "${ENV_FILE}"
     done
   fi
 
   ## binaries
-  SEARCH_BINARIES=$(cat ${OLD_ENV_FILE} | grep -E ${BINARIES})
+  SEARCH_BINARIES=$(grep -E ${BINARIES} "${OLD_ENV_FILE}")
   GITHUB_COUNT=0
 
   if [ "${SEARCH_BINARIES}" != "" ]; then
-    echo "binaries:" >> ${ENV_FILE}
+    echo "binaries:" >> "${ENV_FILE}"
 
     for SEARCH_BINARY in ${SEARCH_BINARIES}
     do
-      BINARY_RESULT=$(echo ${SEARCH_BINARY} | sed s/GITHUB_BINARIES=//g | sed 's/"//g')
+      BINARY_RESULT=$(echo "${SEARCH_BINARY}" | sed s/GITHUB_BINARIES=//g | sed 's/"//g')
 
-      if [ "$(echo ${BINARY_RESULT} | grep -i gcloud)" != "" ]; then
-        echo "  google:" >> ${ENV_FILE}
-        BINARY=$(echo ${BINARY_RESULT} | sed 's/_VERSION//g' | sed 's/=latest//g' |  awk '{print tolower($0)}' | sed 's/gcloud/google-cloud-cli/g')
-        echo  "    - ${BINARY}" >> ${ENV_FILE}
+      if [ "$(echo "${BINARY_RESULT}" | grep -i gcloud)" != "" ]; then
+        echo "  google:" >> "${ENV_FILE}"
+        BINARY=$(echo "${BINARY_RESULT}" | sed 's/_VERSION//g' | sed 's/=latest//g' |  awk '{print tolower($0)}' | sed 's/gcloud/google-cloud-cli/g')
+        echo  "    - ${BINARY}" >> "${ENV_FILE}"
       elif [ ${GITHUB_COUNT} -lt 1 ]; then
-        echo "  github:" >> ${ENV_FILE}
+        echo "  github:" >> "${ENV_FILE}"
         GITHUB_COUNT=1
-        BINARY=$(echo ${BINARY_RESULT} | sed 's/\/releases\/latest//g' | sed 's/:/=/g' | awk -F '/releases/' '{print $1"="$2}' | awk -F '=' '{print $1"="$3"="$2}' | sed 's/==/=/g')
-        echo  "    - ${BINARY}" >> ${ENV_FILE}
+        BINARY=$(echo "${BINARY_RESULT}" | sed 's/\/releases\/latest//g' | sed 's/:/=/g' | awk -F '/releases/' '{print $1"="$2}' | awk -F '=' '{print $1"="$3"="$2}' | sed 's/==/=/g')
+        echo  "    - ${BINARY}" >> "${ENV_FILE}"
       else
-        BINARY=$(echo ${BINARY_RESULT} | sed 's/\/releases\/latest//g' | sed 's/:/=/g' | awk -F '/releases/' '{print $1"="$2}' | awk -F '=' '{print $1"="$3"="$2}' | sed 's/==/=/g')
-        echo  "    - ${BINARY}" >> ${ENV_FILE}
+        BINARY=$(echo "${BINARY_RESULT}" | sed 's/\/releases\/latest//g' | sed 's/:/=/g' | awk -F '/releases/' '{print $1"="$2}' | awk -F '=' '{print $1"="$3"="$2}' | sed 's/==/=/g')
+        echo  "    - ${BINARY}" >> "${ENV_FILE}"
       fi
     done
   fi
 
   ## requirements
-  SEARCH_REQUIREMENTS=$(cat ${OLD_ENV_FILE} | grep -E ${REQUIREMENTS})
+  SEARCH_REQUIREMENTS=$(grep -E ${REQUIREMENTS} "${OLD_ENV_FILE}")
   if [ "${SEARCH_REQUIREMENTS}" != "" ]; then
-    echo "requirements:" >> ${ENV_FILE}
-    REQUIREMENT_RESULT=$(echo ${SEARCH_REQUIREMENTS} | tr ' ' '\n')
+    echo "requirements:" >> "${ENV_FILE}"
+    REQUIREMENT_RESULT=$(echo "${SEARCH_REQUIREMENTS}" | tr ' ' '\n')
 
     for REQUIREMENT in ${REQUIREMENT_RESULT}
     do
-      REQ=$(echo ${REQUIREMENT} | sed 's/_REQUIREMENTS.*//g' |  awk '{print tolower($0)}')
-      REQ_LIST=$(echo ${REQUIREMENT} | sed 's/.*_REQUIREMENTS=//g' | sed 's/"//g')
-      echo "  ${REQ}:" >> ${ENV_FILE}
-      if [ $REQ = "pip" ]; then
-        for REQ_CONTENT in $(cat ${ENV_DIR}/${REQ_LIST} | sed 's/==/=/g')
+      REQ=$(echo "${REQUIREMENT}" | sed 's/_REQUIREMENTS.*//g' |  awk '{print tolower($0)}')
+      REQ_LIST=$(echo "${REQUIREMENT}" | sed 's/.*_REQUIREMENTS=//g' | sed 's/"//g')
+      echo "  ${REQ}:" >> "${ENV_FILE}"
+      if [ "${REQ}" = "pip" ]; then
+        sed 's/==/=/g' "${ENV_DIR}/${REQ_LIST}" | while IFS= read -r REQ_CONTENT
         do
-          echo "    - ${REQ_CONTENT}" >> ${ENV_FILE}
+          echo "    - ${REQ_CONTENT}" >> "${ENV_FILE}"
         done
-        elif [ $REQ = "ansible" ]; then
-        for REQ_CONTENT in $(cat ${ENV_DIR}/${REQ_LIST} | sed 's/^#.*//g' | tr -d '-' | tr -d ' ' | sed 's/name:/ /g' | sed 's/version:/=/g' | sed '/^.*:/ s/./#&/' | tr -d '\n' | tr '#' '\n')
+        elif [ "${REQ}" = "ansible" ]; then
+        for REQ_CONTENT in $(sed 's/^#.*//g' "${ENV_DIR}/${REQ_LIST}" | tr -d '-' | tr -d ' ' | sed 's/name:/ /g' | sed 's/version:/=/g' | sed '/^.*:/ s/./#&/' | tr -d '\n' | tr '#' '\n')
         do
           if [ "$(echo "${REQ_CONTENT}" | grep -E "^roles|collections")" != "" ]; then
-            echo "    ${REQ_CONTENT}" >> ${ENV_FILE}
+            echo "    ${REQ_CONTENT}" >> "${ENV_FILE}"
           else
-            echo "      - ${REQ_CONTENT}" >> ${ENV_FILE}
+            echo "      - ${REQ_CONTENT}" >> "${ENV_FILE}"
           fi
         done
       fi
 
-      rm ${ENV_DIR}/${REQ_LIST}
+      rm "${ENV_DIR}/${REQ_LIST}"
     done
   fi
 
   ## extensions
-  SEARCH_EXTENSIONS=$(cat ${OLD_ENV_FILE} | grep -E ${EXTENSIONS})
+  SEARCH_EXTENSIONS=$(grep -E "${EXTENSIONS}" "${OLD_ENV_FILE}")
 
   if [ "${SEARCH_EXTENSIONS}" != "" ]; then
-      echo "extensions:" >> ${ENV_FILE}
+      echo "extensions:" >> "${ENV_FILE}"
 
-      EXTENSION_RESULT=$(echo ${SEARCH_EXTENSIONS} | tr ' ' '\n' | sed 's/"//g' | sed 's/=/\n/g')
+      EXTENSION_RESULT=$(echo "${SEARCH_EXTENSIONS}" | tr ' ' '\n' | sed 's/"//g' | sed 's/=/\n/g')
       for EXTENSION in ${EXTENSION_RESULT}
       do
-        if [ "$(echo ${EXTENSION} | grep -E "AZ_CLI|HELM" )" != "" ]; then
-          echo "  $(echo ${EXTENSION} | sed 's/_EXTENSIONS//g' | sed 's/AZ_CLI/az/g' | awk '{print tolower($0)}'):" >> ${ENV_FILE}
+        if [ "$(echo "${EXTENSION}" | grep -E "AZ_CLI|HELM" )" != "" ]; then
+          echo "  $(echo "${EXTENSION}" | sed 's/_EXTENSIONS//g' | sed 's/AZ_CLI/az/g' | awk '{print tolower($0)}'):" >> "${ENV_FILE}"
         else
-          echo "    - ${EXTENSION}" >> ${ENV_FILE}
+          echo "    - ${EXTENSION}" >> "${ENV_FILE}"
         fi
       done
   fi
 
-  rm ${OLD_ENV_FILE}
+  rm "${OLD_ENV_FILE}"
 }
 
 ## main
--- render.sh
+++ render.sh
@@ -7,28 +7,29 @@ if [ $# -ne 1 ]; then
   echo "Usage: sh render.sh <DIRECTORY>"
   exit 2
 fi
-SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-ENV_DIR=${1-SCRIPT_DIR}
+
+ENV_DIR=${1-$(dirname "$(readlink -f "$0")")}
 ENV_FILE="${ENV_DIR}/build.yaml"
 BUILD_FILE="${ENV_DIR}/.build"
-DOCKERFILE_TEMPLATE="${SCRIPT_DIR}/Dockerfile.template"
+DOCKERFILE_TEMPLATE="${ENV_DIR}/Dockerfile.template"
 DOCKERFILE="${ENV_DIR}/Dockerfile"
 
 # function
 parse_yaml() {
-  local s='[[:space:]]*' w='[.a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
-  sed -ne "s|,$s\]$s\$|]|" \
-      -e ":1;s|^\($s\)\($w\)$s:$s\[$s\(.*\)$s,$s\(.*\)$s\]|\1\2: [\3]\n\1  - \4|;t1" \
-      -e "s|^\($s\)\($w\)$s:$s\[$s\(.*\)$s\]|\1\2:\n\1  - \3|;p" $1 | \
-  sed -ne "s|,$s}$s\$|}|" \
-      -e ":1;s|^\($s\)-$s{$s\(.*\)$s,$s\($w\)$s:$s\(.*\)$s}|\1- {\2}\n\1  \3: \4|;t1" \
-      -e    "s|^\($s\)-$s{$s\(.*\)$s}|\1-\n\1  \2|;p" | \
-  sed -ne "s|^\($s\):|\1|" \
-      -e "s|^\($s\)-$s[\"']\(.*\)[\"']$s\$|\1$fs$fs\2|p" \
-      -e "s|^\($s\)-$s\(.*\)$s\$|\1$fs$fs\2|p" \
-      -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
-      -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p" | \
-  awk -F$fs '{
+  define_fs=$(echo @|tr @ '\034')
+  local s='[[:space:]]*' w='[.a-zA-Z0-9_]*' fs=${define_fs}
+  sed -ne "s|,${s}\]${s}\$|]|" \
+      -e ":1;s|^\(${s}\)\(${w}\)${s}:$s\[${s}\(.*\)${s},${s}\(.*\)${s}\]|\1\2: [\3]\n\1  - \4|;t1" \
+      -e "s|^\(${s}\)\(${w}\)${s}:$s\[${s}\(.*\)${s}\]|\1\2:\n\1  - \3|;p" "$1" | \
+  sed -ne "s|,${s}}${s}\$|}|" \
+      -e ":1;s|^\(${s}\)-${s}{${s}\(.*\)${s},${s}\(${w}\)${s}:${s}\(.*\)${s}}|\1- {\2}\n\1  \3: \4|;t1" \
+      -e    "s|^\(${s}\)-${s}{${s}\(.*\)${s}}|\1-\n\1  \2|;p" | \
+  sed -ne "s|^\(${s}\):|\1|" \
+      -e "s|^\(${s}\)-${s}[\"']\(.*\)[\"']${s}\$|\1${fs}${fs}\2|p" \
+      -e "s|^\(${s}\)-${s}\(.*\)${s}\$|\1${fs}${fs}\2|p" \
+      -e "s|^\(${s}\)\(${w}\)${s}:${s}[\"']\(.*\)[\"']${s}\$|\1${fs}\2${fs}\3|p" \
+      -e "s|^\(${s}\)\(${w}\)${s}:${s}\(.*\)${s}\$|\1${fs}\2${fs}\3|p" | \
+  awk -F"${fs}" '{
     indent = length($1)/2;
     vname[indent] = $2;
     for (i in vname) {if (i > indent) {delete vname[i]; idx[i]=0}}
@@ -46,7 +47,6 @@ parse_yaml() {
             subname=("_")(vname[1])("_")(vname[2])
           }
           vn=(vn)(vname[i]) }
-
         if (name) {printf("%s=%s\n", vname[0], name)};
         printf("%s%s=%s\n", vname[0], subname, $3);
     }
@@ -54,20 +54,19 @@ parse_yaml() {
 }
 
 # main
-echo > ${DOCKERFILE}
-parse_yaml ${ENV_FILE} > ${BUILD_FILE}
+echo > "${DOCKERFILE}"
+parse_yaml "${ENV_FILE}" > "${BUILD_FILE}"
 
-KEYS=$(cat ${BUILD_FILE} | cut -d '=' -f1 | sort -u)
+KEYS=$(cut -d '=' -f1 "${BUILD_FILE}" | sort -u)
 
 ## generate ARG=VALUE for replacement
 for KEY in ${KEYS}
 do
-  VALUE=$(cat ${BUILD_FILE} | grep -w ${KEY} | cut -d '=' -f2- | sort -u | tr '\n' ';')
-  VALUE_PREFIX=","
-  ARG=$(echo ${KEY} | awk '{print toupper($0)}' | tr -d '.')
+  VALUE=$(grep -w "${KEY}" "${BUILD_FILE}" | cut -d '=' -f2- | sort -u | tr '\n' ';')
+  ARG=$(echo "${KEY}" | awk '{print toupper($0)}' | tr -d '.')
 
-  if [ "$(grep -w "ARG ${ARG}" ${DOCKERFILE_TEMPLATE} | wc -l)" -eq 0 ]; then
-    ARG_N=$(echo ${ARG} | awk -F '_' '{print $1"_N"}')
+  if [ "$(grep -cw "ARG ${ARG}" "${DOCKERFILE_TEMPLATE}")" -eq 0 ]; then
+    ARG_N=$(echo "${ARG}" | awk -F '_' '{print $1"_N"}')
     SED_APPEND_ARGS="${SED_APPEND_ARGS}#${ARG_N}=${ARG}=\"${VALUE}\""
   else
     SED_REPLACE="${SED_REPLACE} s!ARG \<${ARG}\>!ARG ${ARG}=\"${VALUE}\"!g;"
@@ -75,18 +74,18 @@ do
 done
 
 ## replace ARG
-sed -r "${SED_REPLACE}" ${DOCKERFILE_TEMPLATE} > ${DOCKERFILE}
+sed -r "${SED_REPLACE}" "${DOCKERFILE_TEMPLATE}" > "${DOCKERFILE}"
 
 ## append ARG
 if [ "${SED_APPEND_ARGS}" != "" ]; then
-  SED_APPEND_KEYS=$(echo $SED_APPEND_ARGS |  tr '#' '\n' | cut -d '=' -f1 | sort -u | xargs)
+  SED_APPEND_KEYS=$(echo "$SED_APPEND_ARGS" |  tr '#' '\n' | cut -d '=' -f1 | sort -u | xargs)
 
   for SED_APPEND_KEY in ${SED_APPEND_KEYS}; do
-    SED_APPEND_KEY_ARG=$(echo ${SED_APPEND_ARGS} |  tr '#' '\n' | sed -n "s/${SED_APPEND_KEY}=/ARG /p" | sed 's/$/\\n/g' | tr -d '\n')
+    SED_APPEND_KEY_ARG=$(echo "${SED_APPEND_ARGS}" |  tr '#' '\n' | sed -n "s/${SED_APPEND_KEY}=/ARG /p" | sed 's/$/\\n/g' | tr -d '\n')
 
 
-    sed -i "/ARG ${SED_APPEND_KEY}/a ${SED_APPEND_KEY_ARG}" ${DOCKERFILE}
+    sed -i "/ARG ${SED_APPEND_KEY}/a ${SED_APPEND_KEY_ARG}" "${DOCKERFILE}"
   done
 fi
 
-rm ${BUILD_FILE}
+rm "${BUILD_FILE}"
